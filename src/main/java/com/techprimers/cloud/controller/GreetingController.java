205ec797126b6842a4b558917c16a45e64e48866
Valentino Pezzano <valentino_pezzano-github@yahoo.com>
2018-09-18 17:22:47 +0200

    Initial commit.

---
+++ src/main/java/com/techprimers/cloud/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.techprimers.cloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/greeting")
+public class GreetingController {
+	@GetMapping("/{greet}")
+	public String respond(@PathVariable String greet) {
+		return "Welcome and " + greet + " to you!";
+	}
+}
