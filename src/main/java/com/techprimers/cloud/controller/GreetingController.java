ef4c7f3411585a29f9badb46e22d87fb17d9eef7
Valentino Pezzano <valentino_pezzano-github@yahoo.com>
2020-04-19 17:57:43 +0200

    Change path in controller.

--- src/main/java/com/techprimers/cloud/controller/GreetingController.java
+++ src/main/java/com/techprimers/cloud/controller/GreetingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/greeting")
+@RequestMapping("/rest/server/greeting")
 public class GreetingController {
 	@GetMapping("/{greet}")
 	public String respond(@PathVariable String greet) {
